f414911977b3454eaec4ced2ed6cb1fcedd8ff7d
evilgodhedgehog <evilgodhedgehog@gmail.com>
2022-02-05 20:26:55 +0300

    feat: Completed the creation of the news block component

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import'bootstrap/dist/css/bootstrap.min.css';
+import'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/styles.css';
 
 ReactDOM.render(
