a5ee7d56949ae990fbb9dd0a6d9aee87f2f517aa
evilgodhedgehog <evilgodhedgehog@gmail.com>
2022-02-06 20:16:29 +0300

    ref: Refactoring news cell

---
+++ todolist/src/components/NewsCell/NewsCell.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewsCellInfo from './NewsCellInfo';
+
+const NewsCell = (props) => {
+    return (
+        <div className='news news__cell-shape'>
+            <div className='news__cell-background'>
+                <div className='news__cell-background__image position-relative' style={{backgroundImage: `url(${props.background})`}}> 
+                </div>
+            </div>
+            <a href={props.link} className='news__cell-title text-white'>
+                <span>
+                    {props.title}
+                </span>
+            </a>
+            <NewsCellInfo link={props.link} date={props.date} views={props.views}/>
+        </div>
+    );
+};
+
+export default NewsCell;
